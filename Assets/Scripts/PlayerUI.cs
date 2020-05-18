6c1f2c9dda730b288c5f284352a0b7d011f1bcca
leche-hombre <mrjmontgomery@hotmail.com>
2020-05-18 11:38:11 +0100

    Programmatically center crosshair

---
+++ Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour
+{
+
+    public Canvas crossHair;
+    
+
+    void Update()
+    {
+        Vector3 centerScreen = new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0);
+        crossHair.transform.position = centerScreen;
+    }
+}
